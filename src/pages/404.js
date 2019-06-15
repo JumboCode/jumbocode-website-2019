6d7b2767e1ab1b7e3183aa74aa971f0a061e11a3
Jacob Jaffe <jacob.jaffe@tufts.edu>
2019-06-15 16:39:56 -0700

    404 page back button

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Heading, Box } from 'rebass';
+import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 import Section from '../components/Section';
 import Triangle from '../components/Triangle';
@@ -33,11 +34,15 @@ const NotFoundPage = () => (
   <Layout>
     <Section.Container id="404" Background={Background}>
       <Box width={[320, 400, 600]} m="auto">
-        <Heading color="primaryDark" fontSize={['9rem', '13rem', '16rem']}>
+        <Heading color="primaryDark" fontSize={['4rem', '5rem', '6rem']}>
           404
         </Heading>
-        <Heading color="secondary" fontSize={['4rem', '5rem', '6rem']}>
-          {"There isn't anything here"}
+        <Heading color="secondary" fontSize={['1rem', '2rem', '3rem']}>
+          {"Oops! This page doesn't exist..."}
+        </Heading>
+
+        <Heading color="primary" fontSize={['1rem', '2rem', '3rem']}>
+          <Link to="/">Go Back</Link>
         </Heading>
       </Box>
     </Section.Container>
