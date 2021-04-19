d8ac61c7110160fd2c8d23a426d979bddd8be491
agross09 <aleogross@gmail.com>
2021-04-18 20:02:04 -0400

    added about click

--- cypress/integration/site/main.js
+++ cypress/integration/site/main.js
@@ -1,5 +1,12 @@
 describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
-    });
+  it('Does not do much!', () => {
+    expect(true).to.equal(true);
+  });
+
+  it('can visit the main page and navigate to the about page', () => {
+    cy.visit('http://localhost:8000/');
+    cy.get('span')
+      .contains('About')
+      .click();
+  });
 });
